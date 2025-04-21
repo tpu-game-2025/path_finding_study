f8ed64c2df27849177d36aaaf3dc578b957640f2
soltshio <shanshanshien@gmail.com>
2025-04-21 21:15:37 +0900

    完成

--- src/board.cpp
+++ src/board.cpp
@@ -5,7 +5,7 @@ std::map<Mass::status, MassInfo> Mass::statusData =
 {
 	{ BLANK, { 1.0f, ' '}},
 	{ WALL,  {-1.0f, '#'}},
-	{ WATER, { 3.0f, '~'}},
+	{ WATER, { 1.5f, '~'}},
 	{ ROAD,  { 0.3f, '$'}},
 
 	// 動的な要素
@@ -20,7 +20,7 @@ std::map<Mass::status, MassInfo> Mass::statusData =
 bool Board::find(const Point& 始点, const Point& 終点, std::vector<std::vector<Mass>> &mass) const
 {
 	std::multimap<float,Point> q;//オープンリスト(始点からの歩数＋終点とそのマスの距離、マスの位置),(A*では始点からの歩数＋キーを終点とそのマスの距離で管理)
-	mass[始点.y][始点.x].visit(始点, mass[始点.y][始点.x]);
+	mass[始点.y][始点.x].visit(始点, mass[始点.y][始点.x], mass[始点.y][始点.x].getCost());
 
 	//始点をオープンリストに追加
 	q.insert({ Point::distance(始点,終点),始点});
@@ -30,13 +30,12 @@ bool Board::find(const Point& 始点, const Point& 終点, std::vector<std::vect
 		//最も始点から最短距離で行けるマスを調べる
 		Point 現在 = q.begin()->second;
 		Mass& 現在のマス = mass[現在.y][現在.x];//現在のマス
-		int distance = 現在のマス.getSteps();
+		float 現在のマスまでの歩数 = 現在のマス.getSteps();
 		//調べるマスをオープンリストからクローズドリストに
 		q.erase(q.begin());
 		現在のマス.close();
 
 		const static Point 移動量[] = { {-1,0},{1,0},{0,-1},{0,1} };//左、右、上、下
-
 		for (const auto& 移動 : 移動量)//現在地から全方向に調べる
 		{
 			//次のマスの設定
@@ -46,11 +45,11 @@ bool Board::find(const Point& 始点, const Point& 終点, std::vector<std::vect
 			//次の(調査)マスが行けるところかつまだクローズドしてないマスを調べる
 			if (次のマス.canMove() && !次のマス.isClosed())
 			{
-				float 始点からの歩数 = static_cast<float>(distance) + 次のマス.getCost();
+				float 始点からの歩数 = 現在のマスまでの歩数 + 次のマス.getCost();
 
-				int 以前の歩数 = 次のマス.getSteps();
+				float 以前の歩数 = 次のマス.getSteps();
 
-				if (0 <= 以前の歩数)//既に訪れていたなら
+				if (次のマス.isVisited())//既に訪れていたなら
 				{
 					//始点により近い方を登録
 					
@@ -66,25 +65,14 @@ bool Board::find(const Point& 始点, const Point& 終点, std::vector<std::vect
 				}
 
 				//調査マスを訪れ、キューに追加
-				次のマス.visit(現在,現在のマス);
+				次のマス.visit(現在,現在のマス, 次のマス.getCost());
 				q.insert({始点からの歩数+Point::distance(次,終点), 次});
 
 				//次のマスが終点なら...
 				//終点から始点までたどりながらWaypointをつけていく(始点と終点はつけない)
 				if (次 == 終点)
 				{
-					Point& 歩いた場所 = mass[終点.y][終点.x].getParent();
-
-					while (歩いた場所 != 始点)
-					{
-						Mass& 歩いたマス = mass[歩いた場所.y][歩いた場所.x];
-						歩いたマス.set(Mass::WAYPOINT);
-						歩いた場所 = 歩いたマス.getParent();
-					}
-
-					//始点と終点に印をつける
-					mass[始点.y][始点.x].set(Mass::START);
-					mass[終点.y][終点.x].set(Mass::GOAL);
+					chasePath(始点, 終点, mass);//通った道をたどって印をつけていく
 
 					return true;
 				}
@@ -95,4 +83,18 @@ bool Board::find(const Point& 始点, const Point& 終点, std::vector<std::vect
 	return false;
 }
 
+void Board::chasePath(const Point& 始点, const Point& 終点, std::vector<std::vector<Mass>>& mass) const//通った道をたどって印をつけていく
+{
+	Point& 歩いた場所 = mass[終点.y][終点.x].getParent();
 
+	while (歩いた場所 != 始点)
+	{
+		Mass& 歩いたマス = mass[歩いた場所.y][歩いた場所.x];
+		歩いたマス.set(Mass::WAYPOINT);
+		歩いた場所 = 歩いたマス.getParent();
+	}
+
+	//始点と終点に印をつける
+	mass[始点.y][始点.x].set(Mass::START);
+	mass[終点.y][終点.x].set(Mass::GOAL);
+}
--- src/board.h
+++ src/board.h
@@ -47,19 +47,21 @@ public:
 	};
 private:
 	bool is_closed_ = false;//クローズドリストに入れられたか
-	int steps_ = -1;//始点からの歩数
+	bool is_visited_ = false;//訪れたか
+	float steps_ = -1;//始点からの歩数(コストを考慮するのでfloat型で)
 	Point parent_;//どこから来たのか
 	static std::map<status, MassInfo> statusData;
 	status s_ = BLANK;
 
 public:
-	void visit(const Point& parent, Mass& parentMass)//引数にはどのマスから来たかを入れる
+	void visit(const Point& parent, Mass& parentMass, float cost)//引数にはどのマスから来たかを入れる
 	{
-		parent_ = parent; steps_ = parentMass.getSteps() + 1;
+		parent_ = parent; steps_ = parentMass.getSteps() + cost; is_visited_ = true;
 	}
 	void close() { is_closed_ = true; }//クローズドリストに入れる(クローズドにする)
+	bool isVisited() const { return is_visited_; }//訪れたかを教える
 	bool isClosed() const { return is_closed_; }//クローズドしたかを教える
-	int getSteps() const { return steps_; }//始点からの歩数を教える
+	float getSteps() const { return steps_; }//始点からの歩数を教える
 	Point& getParent() { return parent_; }//どこから来たのかを教える
 	void set(status s) { s_ = s; }
 	void set(char c) {// cの文字を持つstatusを検索して設定する（重い）
@@ -94,7 +96,7 @@ private:
 		}
 	}
 
-	//void chasePath(const Point& start, const Point& goal, std::vector<std::vector<Mass>>& mass) const;//通った道をたどって印をつけていく
+	void chasePath(const Point& start, const Point& goal, std::vector<std::vector<Mass>>& mass) const;//通った道をたどって印をつけていく
 
 public:
 	Board(const std::vector<std::string>& map_data) {initialize(map_data);}

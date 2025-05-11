a908c0a3fe32c574a9308b8812773562e69fc84b
totoフォリオ <146707740+Yoshizawa-TPU@users.noreply.github.com>
2025-05-12 02:49:15 +0900

    A*実装

--- src/board.cpp
+++ src/board.cpp
@@ -4,8 +4,8 @@ std::map<Mass::status, MassInfo> Mass::statusData =
 {
 	{ BLANK, { 1.0f, ' '}},
 	{ WALL,  {-1.0f, '#'}},
-	{ WATER, { 3.0f, '~'}},
-	{ ROAD,  { 0.3f, '$'}},
+	{ WATER, { 1.0f, '~'}},
+	{ ROAD,  { 1.0f, '$'}},
 
 	// 動的な要素
 	{ START,	{-1.0f, 'S'}},
@@ -16,8 +16,45 @@ std::map<Mass::status, MassInfo> Mass::statusData =
 };
 
 
-bool Board::find(const Point& 始点, const Point& 終点, std::vector<std::vector<Mass>> &mass) const
+bool Board::find(const Point& 始点, const Point& 終点, std::vector<std::vector<Mass>>& mass) const
 {
+	std::multimap<float, Point> q;
+	mass[始点.y][終点.x].visit(始点, mass[始点.y][始点.x]);
+	q.insert({ Point::distance(始点,終点),始点 });
+	while (!q.empty())
+	{
+		Point 現在 = q.begin()->second;
+		int distance = mass[現在.y][現在.x].getSteps();
+		q.erase(q.begin());
+		mass[現在.y][現在.x].cloase();
+
+		const static Point 移動量[] = { {-1,0},{+1,0},{0,-1},{0,+1} };
+		for (const auto& 移動 : 移動量)
+		{
+			Point 次 = 現在 + 移動;
+			Mass& 次のマス = mass[次.y][次.x];
+			if (map_[次.y][次.x].canMove() && !次のマス.isClosed())
+			{
+				float 始点からの歩数 = static_cast<float>(distance) + 次のマス.getCost();
+				int 以前の歩数 = 次のマス.getSteps();
+				if (0 <= 以前の歩数)
+				{
+					if (以前の歩数 <= 始点からの歩数) continue;
+					for (auto it = q.begin(); it != q.end(); ++it)
+					{
+						if (it->second == 次)
+						{
+							q.erase(it); break;
+						}
+					}
+				}
+				次のマス.visit(現在, 次のマス);
+				q.insert({ 始点からの歩数 + Point::distance(次,終点),次 });
+			}
+			
+		}
+	}
+
 	mass[始点.y][始点.x].set(Mass::START);
 	mass[終点.y][終点.x].set(Mass::GOAL);
 
@@ -25,7 +62,7 @@ bool Board::find(const Point& 始点, const Point& 終点, std::vector<std::vect
 	Point 現在 = 始点;
 	while (現在 != 終点) {
 		// 歩いた場所に印をつける(見やすさのために始点は書き換えない)
-		if (現在 != 始点){mass[現在.y][現在.x].set(Mass::WAYPOINT);}
+		if (現在 != 始点) { mass[現在.y][現在.x].set(Mass::WAYPOINT); }
 
 		// 終点に向かって歩く
 		if (現在.x < 終点.x) { 現在.x++; continue; }
